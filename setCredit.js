e2c948b5abf06cb5e5e3aa419574898dab67086d
Desarrollos-Web-Urquiza <47065942+Desarrollos-Web-Urquiza@users.noreply.github.com>
2019-03-25 09:57:43 -0300

    Add files via upload

---
+++ setCredit.js
@@ -0,0 +1,41 @@
+const conex = require('../app/includes/conex')
+const db = conex.firestore
+
+let uid = "werner"
+
+
+
+
+const getCredit = function(uid){
+  
+
+
+
+    return new Promise((resolve, reject) => {
+
+        var docRef = db.collection("okcredits").doc(uid);
+
+        docRef.get().then(function(doc) {
+            if (doc.exists) {
+                let docu = doc.data()
+                
+                let credit = {
+                        credit: docu.credit
+                    }
+               return resolve(credit)
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        }).catch(function(error) {
+            console.log("Error getting document:", error);
+            return reject()
+        });
+
+    }
+
+}
+
+
+
+module.exports = { getCredit }
